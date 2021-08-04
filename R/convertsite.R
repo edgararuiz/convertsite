13c23926c71dc63a707a0c4c27ead560d723b3ed
Edgar Ruiz <edgararuiz@gmail.com>
2021-08-04 10:33:13 -0500

    creates full file list function

--- R/convertsite.R
+++ R/convertsite.R
@@ -1,8 +1,38 @@
-#' @importFrom purrr map map_chr
+#' @importFrom purrr map map_chr walk
 #' @importFrom magrittr `%>%`
 #' @import stringr
 #' @import fs
 #' @import here
+
+full_file_list <- function(folder, new_folder, exclude_exts = NULL) {
+  fc <- dir_ls(folder, recurse = TRUE)
+  if(!is.null(exclude_exts)) {
+    for(i in 1:length(exclude_exts)) {
+      fc <- fc[path_ext(fc) != exclude_exts[i]]
+    }
+  }
+  if(!dir_exists(new_folder)) dir_create(new_folder)
+  fcs <- path_split(fc)
+  rf <- map(fcs, ~.x[2:length(.x)])
+  rj <- path_join(rf)
+  dj <- as_fs_path(unique(path_dir(rj))) 
+  fd <- dj[dj != "."]
+  dir_create(path(new_folder, fd))
+  rj[is_file(fc)]
+}
+
+#' @export 
+full_file_copy <- function(folder, new_folder, exclude_exts = NULL) {
+  fls <- full_file_list(folder, new_folder, exclude_exts)
+  walk(fls, ~ file_copy(path(folder, .x), path(new_folder, .x)))
+}
+
+#' @export 
+full_file_move <- function(folder, new_folder, exclude_exts = NULL) {
+  fls <- full_file_list(folder, new_folder, exclude_exts)
+  walk(fls, ~ file_move(path(folder, .x), path(new_folder, .x)))
+  dir_delete(folder)
+}
 
 #' @export
 clone_github_repo <- function(github_repo = "rstudio/db.rstudio.com",
@@ -15,20 +45,6 @@ clone_github_repo <- function(github_repo = "rstudio/db.rstudio.com",
   if(dir_exists(repo)) dir_delete(repo)
   sys_command <- paste0("git clone -b ", github_branch," https://github.com/", github_repo, " ", repo)
   system(sys_command)
-}
-
-#' @export 
-full_file_move <- function(folder, new_folder, exclude_exts = NULL) {
-  fc <- dir_ls(folder, recurse = TRUE)
-  if(!dir_exists(new_folder)) dir_create(new_folder)
-  fcs <- path_split(fc)
-  rf <- map(fcs, ~.x[2:length(.x)])
-  rj <- path_join(rf)
-  dj <- as_fs_path(unique(path_dir(rj))) 
-  fd <- dj[dj != "."]
-  dir_create(path(new_folder, fd))
-  fls <- rj[is_file(fc)]
-  walk(fls, ~ file_copy(path(folder, .x), path(new_folder, .x)))
 }
 
 #' @export
