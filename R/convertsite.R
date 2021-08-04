9ca8d59f4fe1442a53160cb8afa67e568d44b67d
Edgar Ruiz <edgararuiz@gmail.com>
2021-08-04 14:18:44 -0500

    Code cleanup

--- R/convertsite.R
+++ R/convertsite.R
@@ -4,38 +4,44 @@
 #' @import fs
 #' @import here
 
-full_file_list <- function(folder, new_folder, exclude_exts = NULL) {
+#' @export 
+full_file_copy <- function(folder, new_folder, exclude_exts = NULL) {
+  if(!dir_exists(new_folder)) dir_create(new_folder)
+  fls <- sanitized_file_list(
+    folder = folder, 
+    exclude_exts = exclude_exts
+    )
+  fd <- folder_list(fls)
+  dir_create(path(new_folder, fd))
+  walk(fls, ~ file_copy(path(folder, .x), path(new_folder, .x)))
+}
+
+#' @export 
+folder_list <- function(file_list) {
+  dj <- as_fs_path(unique(path_dir(file_list))) 
+  dj[dj != "."]
+}
+
+#' @export
+full_file_list <- function(folder, exclude_exts = NULL) {
   fc <- dir_ls(folder, recurse = TRUE)
   if(!is.null(exclude_exts)) {
     for(i in 1:length(exclude_exts)) {
       fc <- fc[path_ext(fc) != exclude_exts[i]]
     }
   }
-  if(!dir_exists(new_folder)) dir_create(new_folder)
+  fc
+}
+
+#' @export
+sanitized_file_list <- function(folder, exclude_exts = NULL) {
+  fc <- full_file_list(
+    folder = folder,
+    exclude_exts = exclude_exts
+  )
   pl <- length(path_split(path_common(fc))[[1]])
   fcs <- path_split(fc)
   rf <- map(fcs, ~.x[(pl+1):length(.x)])
   rj <- path_join(rf)
-  dj <- as_fs_path(unique(path_dir(rj))) 
-  fd <- dj[dj != "."]
-  dir_create(path(new_folder, fd))
   rj[is_file(fc)]
 }
-
-#' @export 
-full_file_copy <- function(folder, new_folder, exclude_exts = NULL) {
-  fls <- full_file_list(folder, new_folder, exclude_exts)
-  walk(fls, ~ file_copy(path(folder, .x), path(new_folder, .x)))
-}
-
-#' @export 
-full_file_move <- function(folder, new_folder, exclude_exts = NULL) {
-  fls <- full_file_list(folder, new_folder, exclude_exts)
-  walk(fls, ~ file_move(path(folder, .x), path(new_folder, .x)))
-  dir_delete(folder)
-}
-
-
-
-
-
