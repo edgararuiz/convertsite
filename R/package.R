146180954dec488c3a1345ab50421bbb0e1f5ec0
Edgar Ruiz <edgararuiz@gmail.com>
2021-11-26 10:08:11 -0600

    Adding Rd parsing routines

--- R/package.R
+++ R/package.R
@@ -8,10 +8,101 @@ package_reference <- function(pkg_folder = "",
       new_name <- path(path_ext_remove(path_file(.x)), ext = "md")
       Rd2md::Rd2markdown(
         .x,
-        here::here("reference", new_name)
+        here::here(reference_folder, new_name)
       )
     }
   )
+
+  # pkg_location <- package_repo_clone("https://github.com/sparklyr/sparklyr")
+  # pkg <- pkgdown::as_pkgdown(pkg_location)
+  # topics <- transpose(pkg$topics)
+  # out <- parse_topic(topics$ml_decision_tree.Rd)
+  # writeLines(out, "test.md")
+
+}
+
+parse_topic <- function(topic) {
+  tag_names <- purrr::map_chr(topic$rd, ~ class(.)[[1]])
+  tags <- split(topic$rd, tag_names)
+  c(
+    paste0("# ", topic$name),
+    parse_section(tags$tag_title),
+    parse_section(tags$tag_description, "## Description"),
+    parse_section(tags$tag_usage, "## Usage"),
+    parse_section_arguments(tags$tag_arguments, "## Arguments"),
+    parse_section(tags$tag_value, "## Value"),
+    parse_section(tags$tag_examples, "## Examples"),
+    parse_section(tags$tag_seealso)
+  )
+}
+
+parse_section <- function(x, title = NULL) {
+  if(!is.null(x)) {
+    c(
+      "\n",
+      title,
+      parse_tag(x[[1]]),
+      "\n"
+    )
+  } else {
+    ""
+  }
+}
+
+parse_section_arguments <- function(x, title = NULL) {
+  if(!is.null(x)) {
+    args_p <- map_chr(x[[1]], ~{
+      et <- .x
+      paste0(map(et, parse_tag), collapse = " | ")
+    })
+    args_ret <- map_lgl(args_p, ~.x != "\n")
+    c(
+      "\n",
+      title,
+      "\n",
+      "Argument      |Description",
+      "------------- |----------------",
+      args_p[args_ret],
+      "\n"
+    )
+  } else {
+    NULL
+  }
+}
+parse_tag <- function(x) {
+  tg_res <- map(x, ~{
+    lv1 <- .x
+    if(length(lv1) > 0) {
+      if(length(lv1) == 1) {
+        res <- parse_line_tag(lv1)
+      } else {
+        lv2 <- map(lv1, parse_line_tag)
+        res <- paste0(lv2, collapse = " ")
+      }
+      if("tag_dontrun" %in% class(lv1)) res <- paste0("```r\n", res, "\n```")
+    } else {
+      res <- ""
+    }
+    res
+  })
+  if(all(map_lgl(x, ~ "RCODE" %in% class(.x)))) {
+    tg_res <- c("```r", tg_res, "```")
+  }
+  paste0(tg_res, collapse = " ")
+}
+
+parse_line_tag <- function(x) {
+  tg_res <- map(x, ~{
+    if(length(.x) > 0) {
+      res <- as.character(.x)
+      if("RCODE" %in% class(.x)) res <- paste0("`", res, "`")
+    } else {
+      res <- ""
+    }
+    res
+  })
+  if("tag_item" %in% class(x)) tg_res <- "\n* "
+  paste0(tg_res, collapse = " ")
 }
 
 #' @export
@@ -76,6 +167,3 @@ package_repo_clone <- function(url = "",
   tf
 }
 
-
-
-# pkg_location <- "/var/folders/l8/v1ym1mc10_b0dftql5wrrm8w0000gn/T/Rtmpc4dBpB/sparklyr"
